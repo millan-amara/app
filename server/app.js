5864b7efda4491dbf8c2ad78ebd87916852b17be
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2023-09-03 15:02:45 +0300

    deploy

--- server/app.js
+++ server/app.js
@@ -89,7 +89,7 @@ app.use(express.static(path.join(__dirname + "/public")))
 
 app.get('/*', function (req, res) {
     res.sendFile(
-        path.join(__dirname, 'public', 'index.html'),
+        path.join(__dirname, '/public', 'index.html'),
         function(err) {
             if (err) {
                 res.status(500).send(err);
