66a4f303b72346a6488d5f1230f8588da1d23d3b
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2023-09-03 14:37:53 +0300

    trying to deploy

--- server/app.js
+++ server/app.js
@@ -101,10 +101,10 @@ app.use('/language', languageRoutes)
 //     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
 // app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
 
-app.use(express.static(path.join(__dirname + "public")))
+app.use(express.static(path.join(__dirname + "/public")))
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
