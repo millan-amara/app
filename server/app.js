4efb7c615b9f215d3fa541d876723e4eb6efdf2f
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2023-09-03 15:10:25 +0300

    deploy

--- server/app.js
+++ server/app.js
@@ -89,7 +89,7 @@ app.use('/language', languageRoutes)
 
 app.get('/*', function (req, res) {
     res.sendFile(
-        path.join(__dirname, "../client/build/index.html"),
+        path.join(__dirname, "./client/build/index.html"),
         function(err) {
             if (err) {
                 res.status(500).send(err);
