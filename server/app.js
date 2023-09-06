08c4e3d55b583e101c1eb36e6a90eb14fb590b2b
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2023-09-06 13:24:20 +0300

    deploying

--- server/app.js
+++ server/app.js
@@ -91,7 +91,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.get('*', function (req, res) {
         res.sendFile(
-            path.join(__dirname, "client", "build", "index.html"),
+            path.join(__dirname, "../", "client", "build", "index.html"),
             function(err) {
                 if (err) {
                     res.status(500).send(err);
