65e32e57f90acc517a21750bbe382c7e9defe7b9
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2023-09-06 15:47:29 +0300

    pushing

--- server/app.js
+++ server/app.js
@@ -98,11 +98,11 @@ if (process.env.NODE_ENV === "production") {
             }
         );
     });
-} else {
-    app.get('/', (req, res) => {
-        res.redirect('/');
-    });
 }
+
+app.get('/', (req, res) => {
+    res.redirect('/');
+});
 
 
 
