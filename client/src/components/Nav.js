108d8132a734a2a57e96573dce427aba5cf49dba
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2023-09-07 00:17:54 +0300

    pushing

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -18,7 +18,7 @@ function Nav({ page }) {
           <div className='px-4 text-sm ml-3 py-1 rounded'>{page}</div>
           <div className='px-4 flex'>
               {/* <button className='hover:bg-green-700 hover:text-white mt-1 px-4 rounded-md duration-300 text-lg'><MdEmail /></button> */}
-              <button onClick={onLogout} className='pt-1 pb-2 px-3 w-24 duration-300 text-sm rounded-full text-white bg-black hover:bg-white hover:text-slate-700 underline'>Log out!</button>
+              <button onClick={onLogout} className='pt-1 pb-2 px-3 w-24 duration-300 text-sm rounded-full text-white bg-slate-700 hover:bg-white hover:text-slate-700 underline'>Log out!</button>
           </div>
         </div>
     </div>
