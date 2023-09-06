050692fb3b06c8d4066ef5b36022ddec704f3105
hughes-curtis <119542134+hughes-curtis@users.noreply.github.com>
2023-09-07 00:09:45 +0300

    pushing

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,17 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Sidebar from '../components/Sidebar'
-import { Link } from 'react-router-dom'
+import { BsFillPersonFill } from 'react-icons/bs';
+import axios from 'axios';
+import backgroundVideo from '../assets/videos/homefive.mp4';
 
 function Dashboard() {
+
+  const [user, setUser] = useState(false);
+
+  useEffect(() => {
+    axios.get('/logged-in')
+    .then((response) => {
+        setUser(response.data)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+  }, [])
+
   return (
     <div>
       <Sidebar />
 
     <div className='ml-60'>
-    <Link to='/languages/create-test' className=''>CREATE TEST</Link>
+    {/* <Link to='/languages/create-test' className=''>CREATE TEST</Link> */}
+    
+
+      <div id='avatar' className='flex justify-center flex-col items-center h-screen text-green-700'>
+          <BsFillPersonFill alt="avatar" className='w-40 rounded-full h-40 mt-4 cursor-pointer' />
+          <p id='welcome' className='italic text-3xl font-bold'>Hello, {user.sName} </p>
+      </div>
+     
     </div>
       
-    </div>
+    </div> 
   )
 }
 
